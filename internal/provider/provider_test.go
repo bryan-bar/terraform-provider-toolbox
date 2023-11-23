fb70745361239f0d59b5b561ff8441aa3d17fc3e
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-11-23 05:01:47 +0000

    Update version

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -16,7 +16,7 @@ func protoV6ProviderFactories() map[string]func() (tfprotov6.ProviderServer, err
 func providerVersion() map[string]resource.ExternalProvider {
 	return map[string]resource.ExternalProvider{
 		"toolbox": {
-			VersionConstraint: "0.1.4",
+			VersionConstraint: "0.2.0",
 			Source:            "EnterpriseDB/toolbox",
 		},
 	}
